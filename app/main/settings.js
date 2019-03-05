19775461da93d01bab090785c24d686b07c9b1ee
Rulee Chen <rulee@live.cn>
2019-03-05 15:30:23 +0800

    include settings

---
+++ app/main/settings.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const Store = require('electron-store');
+const dataPath = require('../lib/dataPath');
+const brand = require('../brand');
+
+let _appSettings;
+module.exports = {
+  get appSettings() {
+    if (!_appSettings) {
+      const settingsPath = path.resolve(dataPath(), `./${brand.brandAppId}/settings`);
+      _appSettings = new Store({
+        cwd: settingsPath,
+        name: 'app',
+      });
+    }
+    return _appSettings;
+  }
+};
