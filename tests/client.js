5a9e58cb63c831a6e84dc84389235363f29d3a0a
Rulee Chen <rulee@live.cn>
2019-03-22 16:15:20 +0800

    shrink project size

--- tests/client.js
+++ tests/client.js
@@ -1,70 +1,10 @@
-const fs = require('fs');
-const path = require('path');
 const connector = require('./connector');
 
-//---------------------------------------------------------------------
 const mainWindowIndex = 0;
 
 connector.getWindows()
   .then((wins) => (
     wins[mainWindowIndex].webContents.executeScript('alert("hello everyone!")')
-  ))
-  .catch((err) => {
-    console.error(err);
-  });
-
-//---------------------------------------------------------------------
-
-const isMainWindowScript = `
-function() {
-  const isMainWindow = (location.pathname === '/_');
-  return {
-    isMainWindow,
-  };
-}()
-`;
-
-const insertHtmlScript = `
-  function appendHtml(el, html) {
-    const temp = document.createElement('div');
-    temp.style.display = 'none';
-    temp.innerHTML = html;
-    while (temp.firstChild) {
-      el.appendChild(temp.firstChild);
-    }
-  }
-  appendHtml(document.body, '<div id="viewport" style="position: absolute; bottom: 20px; right: 20px; width: 300px; height: 500px;"></div>');
-`;
-const filePath = path.resolve(__dirname, './rcphone.js');
-const phoneScript = fs.readFileSync(filePath, { encoding: 'utf8' });
-
-connector.getWindows()
-  .then((wins) => (
-    Promise.all([
-      wins[mainWindowIndex]
-        .webContents
-        .inspect()
-        .then((res) => {
-          console.log(res);
-        }),
-
-      wins[mainWindowIndex]
-        .webContents
-        .runQuery(isMainWindowScript)
-        .then((res) => {
-          console.log(res);
-        }),
-
-      wins[mainWindowIndex]
-        .webContents
-        .executeScript(insertHtmlScript)
-        .then(() => (
-          wins[mainWindowIndex].webContents.executeScript(phoneScript)
-        ))
-        .then((res) => {
-          console.log(res);
-        }),
-    ])
   ))
   .catch((err) => {
     console.error(err);
